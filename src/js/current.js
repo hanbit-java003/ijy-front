2f28d88d6f79c80aa562ef93736c8a2e14bb7e4e
ijy <sereinn@naver.com>
2017-07-12 17:57:35 +0900

    current exhibition page

---
+++ src/js/current.js
@@ -0,0 +1,15 @@
+require('bootstrap');
+require('../less/current.less');
+require('../less/common.less');
+
+/*
+function initSlider() {
+    for (var i=0; i<contents.length; i++) {
+        var html = templete(contents[i]);
+
+        $('.exhibition-contents').append(html);
+    }
+
+    $('.main-contents > li:first-child').addClass('active');
+}
+*/
